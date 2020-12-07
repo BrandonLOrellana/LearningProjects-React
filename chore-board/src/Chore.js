09614a308369d3fe2d26d6cd96338c99af1061d5
BrandonLOrellana <brandon.louis.orellana@gmail.com>
2020-12-07 02:00:58 -0800

    Finish button now updates bool

--- chore-board/src/Chore.js
+++ chore-board/src/Chore.js
@@ -2,11 +2,15 @@ import './App.css';
 
 function Chore(props) {
     console.log(props);
+    function setFinished(){
+        props.data.finished = true;
+        console.log(props.data);
+    }
     return (
         <div className="Chore">
             <h2>{props.data.name}</h2>
             <p>{props.data.chore}</p>
-            <button>Finished</button>
+            <button onClick={setFinished}>Finished</button>
         </div>
     );
 }
